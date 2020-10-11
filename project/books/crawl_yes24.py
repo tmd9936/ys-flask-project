05a4298f0594dfaae0622b030eb309aa235a0ab5
tmd9936 <qor9936@gmail.com>
2020-10-12 00:03:56 +0900

    책 등록 플라스크까지 동작 확인x

--- project/books/crawl_yes24.py
+++ project/books/crawl_yes24.py
@@ -2,6 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 
 USER_SHOP_URL = "UsedShop"
+BOOK_DETAIL_URL = "http://www.yes24.com/Product/Goods/"
+IMAGE_URL = "http://image.yes24.com/goods/"
+IMAGE_SIZE_URL = "/L"
+
 
 def search_book(search_str, page=1):
     header = {"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"}
@@ -46,3 +50,52 @@ def search_book(search_str, page=1):
         "src_list":src_list,
         "url_list":url_list
     }
+
+
+def crwal_index(book_num):
+    header = {"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"}
+    url = BOOK_DETAIL_URL + str(book_num)
+    res = requests.get(url, headers = header)
+
+    bs = BeautifulSoup(res.text, "lxml")
+
+    indexes = bs.select_one("#infoset_toc .txtContentText")
+    # indexs = bs.find("div", ("gd_infoSet", "gd_infoSetCrop", "infoMoreOn"))
+
+    # print(indexs.contents)
+
+    # 책 제목, 지은이 등등 정보 추가 가져오기
+
+    # 나중에.. 관리자가 사람들이 등록한 책 보고 목차 설된것 중에 좋은거 있으면 그 책의 베이스 테이블로 선택해서 
+    # 나중에 등록한 사용자가 뎁스 설정같은거 안해도 되는 시스템
+
+
+    index_list = list()
+    
+    if indexes is None:
+        return None
+
+    for content in indexes.contents:
+        if str(content).find("<br/>") == -1:
+            index_list.append(str(content).strip())
+
+    image = IMAGE_URL + str(book_num) + IMAGE_SIZE_URL
+    name = bs.select_one(".gd_titArea .gd_name").text
+    auth = bs.select_one(".gd_pubArea .gd_auth").text.strip()
+    publisher = bs.select_one(".gd_pubArea .gd_pub").text.strip()
+    date = bs.select_one(".gd_pubArea .gd_date").text.strip()
+
+    result = {
+        "indexes":index_list,
+        "image":image,
+        "name":name,
+        "auth":auth,
+        "publisher":publisher,
+        "date":date
+    }
+
+    return result
+
+
+
+# print(crwal_index(74269921231231))
--- project/books/routes.py
+++ project/books/routes.py
@@ -1,5 +1,6 @@
 from . import *
-from .crawl_yes24 import search_book
+from .crawl_yes24 import search_book, crwal_index
+from datetime import datetime
 
 @books_blueprint.route("/list", methods=["GET"])
 @login_required
@@ -18,4 +19,56 @@ def book_search_result(search_str, page):
     lis = search_book(search_str, page)
 
     return lis
+    
+
+@books_blueprint.route("/join/<book_num>", methods=["GET","POST"])
+@login_required
+def book_join(book_num):
+    if book_num is not None and book_num != "":
+        if request.method == "GET":
+            result = crwal_index(book_num)
+
+            if result is None:
+                flash("존재하는 책이 아닙니다.")
+                return redirect(url_for("book.book_search"))
+
+            book_db = mongo.db.book
+
+            current_utc_time = round(datetime.utcnow().timestamp()* 1000)
+
+            book_data = {
+                "member_id":session["id"],
+                "name":result.get("name"),
+                "publisher":result.get("auth"),
+                "publishdate":result.get("date"),
+                "perallstudy":0,
+                "lastmodify":current_utc_time,
+            }
+
+            doc = book_db.insert_one(book_data)
+            book_id = doc.inserted_id
+
+            book_indexes_db = mongo.db.indexes
+
+            indexes_data = list()
+            for index in result.get("indexes"):
+                indexes = dict()
+                indexes["bookid"] = book_id
+                indexes["memberid"] = session["id"]
+                indexes["name"] = index
+                indexes["perstudy"] = 0
+                indexes["latestdate"] = current_utc_time
+                
+                indexes_data.append(indexes)
+
+            book_indexes_db.insert_many(indexes_data)
+
+      
+            return render_template("book/join.html", title="책수정", result=result)
+        else:
+            pass
+    else:
+        flash("책 번호를 정확히 입력하세요.")
+        redirect(url_for("book.book_search"))
+    
     
